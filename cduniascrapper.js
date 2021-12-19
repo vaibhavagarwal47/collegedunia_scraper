7484fbf9b6c05d7917448f3b9ff48ebe8ccfaa94
Vaibhav Agarwal <58652650+vaibhavagarwal47@users.noreply.github.com>
2021-12-19 15:21:45 +0530

    Update cduniascrapper.js

--- cduniascrapper.js
+++ cduniascrapper.js
@@ -22,3 +22,31 @@ const puppeteer = require("puppeteer");
 
   await browser.close();
 })();
+
+let collegeurl = "https://collegedunia.com/college/28754-manajiraje-bhosale-technical-campus-faculty-of-engineering-mbt-sangli";
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setUserAgent(
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"
+  );
+  await page.goto(collegeurl);
+  await page.waitForTimeout(2500);
+
+  let info = await page.evaluate(() => {
+    if (document.querySelector(".cdcms_college_highlights>p:nth-child(3)")){
+        return {
+          'summary': document.querySelector(".cdcms_college_highlights>p:nth-child(3)").textContent,
+          'elig': document.querySelector("tbody.jsx-2675951502").textContent
+        }
+      }
+      else{
+        return {
+          'summary': 'Not Present',
+          'elig': document.querySelector("tbody.jsx-2675951502").innerText
+        }
+      }
+    });
+  console.log(info);
+  await browser.close();
+})();
